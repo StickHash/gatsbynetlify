552fdd640dc251447b3e3fa95b2f1b613e74e3e8
StickHash <chrisb1981@orange.fr>
2019-11-30 13:30:49 +0100

    Second Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mon nouveau site avec Gatsby`,
+    description: `Le début de quelques chose.`,
+    author: `@StickHash`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
